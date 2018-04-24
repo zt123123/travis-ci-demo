001bd18ae2d2db2ab6a664ec3a5195bd3810ce10
zt123123 <249812928@qq.com>
2018-04-24 22:50:03 +0800

    xxxxxxx

--- hello.test.js
+++ hello.test.js
@@ -6,7 +6,7 @@ var hello = require('./hello');
 describe('run hello.js', function() {
 	before(function(){
 		console.log('start');
-	})
+	});
   it('should return "Hello World"', function() {
     expect(hello()).to.equal('Hello Node');
   });
