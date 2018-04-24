42f90e8a5c169a64fabe558824b2265c7f62ba1a
zt123123 <249812928@qq.com>
2018-04-24 22:47:54 +0800

    addd

--- hello.test.js
+++ hello.test.js
@@ -4,6 +4,9 @@ var expect = require('chai').expect;
 var hello = require('./hello');
 
 describe('run hello.js', function() {
+	before(function(){
+		console.log('start');
+	})
   it('should return "Hello World"', function() {
     expect(hello()).to.equal('Hello Node');
   });
